4c12e427d5ae728af7eaa82dce146021bc2f46d6
elisamorenoo <elisamoreno.cartoons@gmail.com>
2022-04-08 14:46:15 -0400

    añadido tooltip y popover en parte

--- assets/js/miJS.js
+++ assets/js/miJS.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    //smooth scroll
     $("a").on('click', function(event) {
 
         if (this.hash !== "") {
@@ -15,5 +15,39 @@ $(document).ready(function() {
             });
         }
     });
+
+    //evento alerta enviar Correo
+    $("#enviar").click(function() {
+        alert("Recuerda haber llenado todos los campos...");
+    });
+
+
+    //on al pasar el mouse sobre p
+    $("p")
+        .on("mouseenter", function() {
+            $(this).css({
+                "background-color": "yellow",
+                "font-weight": "bolder"
+            });
+        })
+        .on("mouseleave", function() {
+            var styles = {
+                backgroundColor: "",
+                fontWeight: ""
+            };
+            $(this).css(styles);
+        });
+
+    //tooltip
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    //popover
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
 
 })
